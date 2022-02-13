73a5462e271ca82c276b06e5258950ab7b73223d
PhdInSpd <chaver.yadim@gmail.com>
2022-02-12 16:40:01 -0800

    adding initial quickSort

---
+++ Module8/integerSort/quickSort/quickSort.cpp
@@ -0,0 +1,21 @@
+// https://www.youtube.com/watch?v=Hoixgm4-P4M
+#include <iostream>
+#include <vector>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+// Sorts theArray[first..last].
+void quickSort(std::vector<int>& theArray, int first , int last ) {
+    if (first < last) {
+        /*Choose a pivot item p from theArray[first..last]
+        Partition the items of theArray[first..last] about p*/
+        int pivotIndex = last;
+
+        // The partition is theArray[first..pivotIndex..last]
+        quickSort(theArray, first, pivotIndex - 1) // Sort S 1
+        quickSort(theArray, pivotIndex + 1, last) // Sort S 2
+    }
+// If first >= last, there is nothing to do
